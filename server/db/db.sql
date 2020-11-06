e0568d13b221803ab2c30ba30528503df09e6672
surafelx <surafelyimam.sy@gmail.com>
2020-11-06 09:51:24 +0300

    Initial commit (Folder structure and DB)

---
+++ server/db/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE yelp;
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY, 
+    name varchar(50) NOT NULL,
+    location VARCHAR(50) NOT NULL, 
+    price_range INT NOT NULL CHECK(price_range >=1 and price_range <=5)
+);
+
+INSERT INTO restaurants (id, name, location, price_range) values (123, 'mcdonalds', 'new yorks', 3)
